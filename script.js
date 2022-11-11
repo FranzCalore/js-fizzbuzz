0c6529abd8cf4cabd2f8d23590640b1aa566ad40
FranzCalore <franzkaiser1993@gmail.com>
2022-11-11 13:18:39 +0100

    terminato l'esercizio con il bonus

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-for (let i=1; i<=100; i++){
-    if (i%3==0 && i%5==0){
-        console.log ("FizzBuzz");
-    }
-    else if (i%3==0){
+let containerPrincipale=document.querySelector(".mio-container");
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        containerPrincipale.innerHTML += '<div class="square"><p>FizzBuzz</p></div>';
+        console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+        containerPrincipale.innerHTML += '<div class="square"><p>Fizz</p></div>';
         console.log("Fizz");
-    }
-    else if (i%5==0){
+    } else if (i % 5 == 0) {
+        containerPrincipale.innerHTML += '<div class="square"><p>Buzz</p></div>';
         console.log("Buzz");
+    } else {
+        containerPrincipale.innerHTML += '<div class="square"><p>' + i + '</p></div>';
+        console.log(i)
     }
-    else {
-    console.log(i)}
 }
